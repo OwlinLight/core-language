ca5b07832242e062a6b0beafece255f968df7e67
OwlinLight <42697056+OwlinLight@users.noreply.github.com>
2023-08-17 16:11:00 -0400

    initial import

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "scanner.h"
+#include "core.h"
+#include "parser.h"
+#include "executor.h"
+//#include "printer.h"
+//#include "semantic.h"
+#include "tree.h"
+
+int main(int argc, char *argv[]) {  
+
+    scanner_open(argv[1]);
+
+    struct nodeProcedure p;
+
+    //printf("PARSING\n");
+
+    parseProcedure(&p);
+
+//    printf("PRINTING\n");
+//    printf("RUNNING\n");
+
+//    semanticProcedure(&p);
+//    printProcedure(&p);
+
+    scanner_close();
+
+    // Read inputs
+    scanner_open(argv[2]);
+
+    executeProcedure(&p);
+  
+  // Scanning, parsing, printing is done, release memory
+    scanner_close();
+    return 0;
+}
